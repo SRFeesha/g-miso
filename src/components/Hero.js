59378f734bd0f8ecf4c230be69a7225a078fd071
SRFeesha <SRFeesha@gmail.com>
2020-07-25 12:44:26 +0200

    added hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,26 @@
+import React from 'react'
+// import logo from '../img/logo.svg'
+// import facebook from '../img/social/facebook.svg'
+// import instagram from '../img/social/instagram.svg'
+import heroImage from '../img/Miso_Hero_Image.png'
+// import heroImage from '../img/social/instagram.svg'
+import heroStyle from "./Hero.module.css"
+
+console.log(heroImage);
+
+const Hero = class extends React.Component {
+  render() {
+    return (
+        <section>
+            <h1>
+                Miso<span> è una </span><br />associazione culturale no-profit <span>fondata a </span>Brescia.
+            </h1>
+            <p className="sub">
+                Siamo più di sessanta ragazzi: attori, filmmakers, registi o semplici appassionati
+            </p>
+            <img alt= "miso all'opera" src={heroImage} className={heroStyle.img} />
+        </section>
+    )
+  }
+}
+export default Hero
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Hero from "../components/Hero";
+
+const Home = () => (
+  <Layout>
+    <div>
+      
+      <Hero />
+    </div>
+  </Layout>
+);
+
+export default Home;

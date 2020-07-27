bf38940e481fa83b423af8665b22eb6e2938e0da
SRFeesha <SRFeesha@gmail.com>
2020-07-27 16:42:41 +0200

    Added cosa facciamo, unisciti a noi e motivazioni

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
+// import Spacer from "../components/Spacer";
+import CosaFacciamo from "../components/CosaFacciamo";
+import Motivazioni from "../components/Motivazioni";
 
 const Home = () => (
   <Layout>
     <div>
-      
       <Hero />
+      {/* <Spacer h="400px" /> */}
+      <CosaFacciamo />
+      <Motivazioni />
     </div>
   </Layout>
 );

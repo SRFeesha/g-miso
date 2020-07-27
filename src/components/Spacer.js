66c34ddcebcc27d440063bff807eea063136eb1f
SRFeesha <SRFeesha@gmail.com>
2020-07-27 16:41:31 +0200

    new utility: Spacer

---
+++ src/components/Spacer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Spacer = (props) => {
+  return <div style={{height: props.h}}></div>;
+//   return <div style="height: {props.h} + 'em'}"></div>;
+};
+
+export default Spacer;

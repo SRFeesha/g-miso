07d6cd4665fccb4858f4ec1e91fac4dfc5e72ad1
SRFeesha <SRFeesha@gmail.com>
2020-07-31 19:13:22 +0200

    Added link to social button

--- src/components/ButtonSocial.js
+++ src/components/ButtonSocial.js
@@ -2,10 +2,11 @@ import React from 'react'
 import style from './ButtonSocial.module.css'
 import Button from "@material-ui/core/Button";
 
+
 const ButtonSocial = (props) => {
     return (
-        <Button className={style.btn}>
-            <img src={props.logo} />
+        <Button className={style.btn} onClick={() => {  window.open(props.href, "_blank"); }}>
+            <img src={props.logo} alt={props.alt}/>
         </Button>
     )
 }
--- src/components/Social.js
+++ src/components/Social.js
@@ -2,7 +2,6 @@ import React from "react";
 import Section from "./Section";
 import ShadowTitle from "./ShadowTitle";
 import Spacer from "./Spacer";
-import SocialStyle from "./Social.module.css";
 import LogoInsta from "../img/Instagram Logo.svg";
 import LogoMail from "../img/Mail Logo.svg";
 import LogoFb from "../img/Facebook Logo.svg";
@@ -14,10 +13,10 @@ const Social = () => {
     <Section>
       <Spacer h="10rem" />
       <Container>
-        <Row debug>
-          <Col md={5} offset={{ lg: 2 }} debug>
+        <Row>
+          <Col md={5} offset={{ lg: 2 }}>
             <ShadowTitle title="Restiamo in contatto" />
-            <p class="giant transparent">
+            <p className="giant transparent">
               Non lasciamo idee nel cassetto e ogni progetto è un’opportunità
               per rinnovarsi e sperimentare. Puoi restare aggiornato sulle nuove
               attività tramite i social Facebook e Instagram o direttamente
@@ -28,14 +27,14 @@ const Social = () => {
           </Col>
         </Row>
         <Spacer h="4rem" />
-        <Row debug>
-          <Col xs={2} md={1} offset={{ lg: 2}} debug>
-            <ButtonSocial logo={LogoInsta} />
+        <Row>
+          <Col xs={2} md={1} offset={{ lg: 2}}>
+            <ButtonSocial logo={LogoInsta} href="https://www.instagram.com/studio.miso/" />
           </Col>
-          <Col xs={2} md={1} offset={{ lg: 1, xs:1 }} debug>
-            <ButtonSocial logo={LogoFb} />
+          <Col xs={2} md={1} offset={{ lg: 1, xs:1 }}>
+            <ButtonSocial logo={LogoFb} href="https://www.facebook.com/studiomisobrescia/" />
             </Col>
-            <Col xs={2} md={1} offset={{ lg: 1, xs:1 }} debug>
+            <Col xs={2} md={1} offset={{ lg: 1, xs:1 }}>
             <ButtonSocial logo={LogoMail} />
             </Col>
         </Row>

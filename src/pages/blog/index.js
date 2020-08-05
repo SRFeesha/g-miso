d6b19e4ae2311c10d919a59be79abe5ecc25ead6
SRFeesha <SRFeesha@gmail.com>
2020-08-05 15:06:06 +0200

    featured post layout

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,38 +1,31 @@
-import React from 'react'
-
-import Layout from '../../components/Layout'
-import BlogRoll from '../../components/BlogRoll'
+import React from "react";
+import Layout from "../../components/Layout";
+import BlogRoll from "../../components/BlogRoll";
+import FeaturedPost from "../../components/FeaturedPost";
+import style from "../../styles/blog.module.css";
+import { Container, Row, Col } from "react-grid-system";
 
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <div
-          className="full-width-image-container margin-top-0"
-          style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
-          }}
-        >
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
-              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
-              color: 'white',
-              padding: '1rem',
-            }}
-          >
-            Latest Stories
-          </h1>
-        </div>
-        <section className="section">
-          <div className="container">
-            <div className="content">
+        <Container>
+          <h1 className={style.sectionHeading}>Articoli in evidenza</h1>
+          <Row>
+            <Col lg={6} offset={{ lg: 1 }}>
+              <FeaturedPost />
+            </Col>
+          </Row>
+          
+          <h1 className={style.sectionHeading}>Ultimi articoli</h1>
+          <Row>
+            <Col lg={2}></Col>
+            <Col lg={6}>
               <BlogRoll />
-            </div>
-          </div>
-        </section>
+            </Col>
+          </Row>
+        </Container>
       </Layout>
-    )
+    );
   }
 }

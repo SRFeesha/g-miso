9a67724fec8d6a08108ca780e1f9c2500bb51744
SRFeesha <SRFeesha@gmail.com>
2020-08-06 17:18:44 +0200

    init page

---
+++ src/pages/trattamento-dei-dati.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const PrivacyPolicyPage = () => (
+  <Layout>
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        OGGETTO: acquisizione del consenso al trattamento dei dati personali ai sensi del D.Lgs 196/2003 
+      </p>
+      <p>
+        Io sottoscritto/a
+        offro il mio consenso al trattamento dei dati personali per le attività dell’Associazione.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default PrivacyPolicyPage

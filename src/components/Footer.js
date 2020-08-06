5c1d66f1735f403acb350db669f37427410c0727
SRFeesha <SRFeesha@gmail.com>
2020-08-06 17:18:14 +0200

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,115 +1,61 @@
-import React from 'react'
-import { Link } from 'gatsby'
-
-import logo from '../img/logo.svg'
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import style from "./Footer.module.css";
+import logo from "../img/logo/logo_white_48.png";
+import { Container, Row, Col } from "react-grid-system";
+import Spacer from "./Spacer";
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        {/* <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
-          />
-        </div> */}
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div style={{ maxWidth: '100vw' }} className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
-                    <li>
-                      <Link to="/" className="navbar-item">
-                        Home
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/about">
-                        About
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact/examples">
-                        Form Examples
-                      </Link>
-                    </li>
-                    <li>
-                      <a
-                        className="navbar-item"
-                        href="/admin/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Admin
-                      </a>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4">
-                <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
-                  </ul>
-                </section>
-              </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <footer className={style.footer}>
+        <Spacer h={11} />
+        <hr className="line" />
+        <Spacer h={9} />
+        <Container>
+          <Row>
+            <Col lg={2} offset={{ lg: 1 }} className={style.col}>
+              <Link to="/">Home</Link>
+              <Link to="/blog">Blog</Link>
+              <a href="https://www.instagram.com/studio.miso/">Instagram</a>
+              <a href="https://www.facebook.com/studiomisobrescia/">Facebook</a>
+              <a href="mailto:studiomisobrescia@gmail.com">Inviaci una mail</a>
+            </Col>
+            <Col lg={2} className={style.col} >
+              <Link to="/">Privacy Policy</Link>
+              <Link to="/">Trattamento dei dati</Link>
+              <Link to="/">Organigramma Miso</Link>
+              <Link to="/">Statuto dell'associazione</Link>
+            </Col>
+            <Col lg={3} className={style.col} >
+              <p>
+                Miso · Associazione di promozione sociale <br />
+                Via Moretto 84 · 25121 · Brescia (BS)
+                <br />
+                Codice Fiscale 98198280178
+              </p>
+              <Link to="/">
+                <img src={logo} alt="logo miso" className={style.logo} />
+              </Link>
+            </Col>
+          </Row>
+          <Row>
+            <Col 
+              // offset={{lg: 5}}
+            >
+            <p className={style.credit}>
+              Made with Gatsby by{" "}
+              <a className="discrete-link" href="https://youcancallmeben.design">
+                Beniamino Marini
+              </a>
+            </p>
+            </Col>
+          </Row>
+        </Container>
+        <Spacer h={6} />
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;

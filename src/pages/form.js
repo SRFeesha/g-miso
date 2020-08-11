1c6644c02ad01d435146f423dfed1fa7f493918d
SRFeesha <SRFeesha@gmail.com>
2020-08-11 14:43:29 +0200

    Created new test page

---
+++ src/pages/form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FormContainer from '../components/form/FormContainer'
+
+const FormDemo = (props) => {
+    return(
+        <FormContainer />
+    )
+}
+
+export default FormDemo;

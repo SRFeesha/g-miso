cd776cbd2e89b9634998b9c09f0d50faa30b4722
SRFeesha <SRFeesha@gmail.com>
2020-08-29 19:42:06 +0200

    formatted gsap file

--- src/components/ShadowTitle.js
+++ src/components/ShadowTitle.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import style from './ShadowTitle.module.css'
 import Spacer from './Spacer'
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 const tl = gsap.timeline({
   scrollTrigger: {
-      trigger: ".shadow+title",
-      scrub: true,
-      markers: true
-  }
-}); 
-tl.to(".shadow+title .shadow", {x: 2000})
+    trigger: '.shadow+title',
+    scrub: true,
+    markers: true,
+  },
+})
+tl.to('.shadow+title .shadow', { x: 2000 })
 
 const shadowTitle = (props) => {
   return (

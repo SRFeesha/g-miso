18aae9b7c0394ee76a3fef6745027b2c70b9949e
SRFeesha <SRFeesha@gmail.com>
2020-09-01 12:06:32 +0200

    different version of the component

--- src/components/ShadowTitle_orig.js
+++ src/components/ShadowTitle_orig.js
@@ -1,29 +1,13 @@
 import React from 'react'
-import style from './ShadowTitle.module.css'
+// import style from './ShadowTitle.module.css'
 import Spacer from './Spacer'
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
-gsap.registerPlugin(ScrollTrigger)
-
-const tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.shadow+title',
-    scrub: true,
-    markers: true,
-  },
-})
-tl.to('.shadow+title .shadow', { x: 2000 })
-
-
-
 
 const shadowTitle = (props) => {
   return (
     <div>
       <Spacer h={12} />
 
-      <div className="shadow+title">
+      <div className="shadow-title-parent">
         <div className="shadow">{props.title}</div>
         <div className="shadow-title">{props.title}</div>
       </div>

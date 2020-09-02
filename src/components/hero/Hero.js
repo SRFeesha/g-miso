7a0769b91d1372aaa1112a0b0bb38dfa129a250d
SRFeesha <SRFeesha@gmail.com>
2020-09-02 17:06:14 +0200

    animation for the headline

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,31 +2,63 @@ import React, { useEffect, useRef } from "react";
 import style from "./Hero.module.css";
 import Spacer from "../Spacer";
 import { gsap } from "gsap"
+import Splitting from "splitting";
+import 'splitting/dist/splitting.css'
+import 'splitting/dist/splitting-cells.css'
+
 import heroImage from '../../img/Hero-2x-scura.png'
 
 const Hero = () => {
-  let imgContainerRef = useRef(null);
+  let imgContainerRef = useRef(null)
+  let titleRef = useRef(null)
+  let subtitleRef = useRef(null)
+  const tl = gsap.timeline()
   
-  // create and pause the timeLine
+  useEffect(() => {
+    if (titleRef) {
+      let results = Splitting({target: titleRef, by: 'lines',})
+      
+      results.forEach((splitResult) => {
+        const wrappedLines = splitResult.lines.map((wordsArr) => `
+        <div class="mask-line">
+          ${wordsArr.map((word) => `${word.outerHTML}<span class="whitespace"> 
+         </span>`).join('')}
+        </div>`).join('');
+        splitResult.el.innerHTML = wrappedLines;
+      });
+    }
+  }, [titleRef])
+
   useEffect(() => {
     const hero = imgContainerRef.firstElementChild;
-    // obviously this should be a timeline
-    gsap.from(imgContainerRef, 1.2, { y: 1280, ease: "ease-out" }, "Start")
-    gsap.from(hero, 2, { scale: 1.6, ease: "ease-out" }, 1.4)
-  }, []);
+    const maskRef = titleRef.querySelectorAll(".mask-line")
+    let lineRefs = []
 
+    maskRef.forEach((l, i) => {
+      lineRefs[i] = l.querySelectorAll(".word")
+      i++
+    })
+
+    tl.from(imgContainerRef, 1.2, { y: 1280, ease: "ease-out" })
+      .from(hero, 2, { scale: 1.6, ease: "ease-out" }, .2)
+      .to(titleRef, 0, { autoAlpha: 1}, 1)
+      .staggerFrom(lineRefs, .4, { yPercent: 100, ease: 'ease-out', autoAlpha: 0 }, .05, 1)
+      .fromTo(subtitleRef, .3, {yPercent: 50, ease: 'ease-out', autoAlpha: 0}, {yPercent: 0, autoAlpha: .5}, 1.5)
+  }, []);
+    
   return (
     <section id="hero">
       <Spacer h={11} />
       <div className={style.hero}>
-        <div className={style.headline}>
-          <h1 className={style.title}>
-            Miso<span> è una </span>
+        <div className={style.headline} >
+          <h1 className={style.title} ref={el => titleRef = el} data-splitting>
+            Miso<em className={style.low}> è una </em>
             <br />
-            associazione culturale no-profit <span>fondata a </span>
+            associazione culturale no-profit <em className={style.low}>fondata a </em>
             Brescia.
           </h1>
-          <p className={style.subtitle}>
+          <Spacer h={5} />
+          <p className={style.subtitle} ref={el => subtitleRef = el}>
             Siamo più di sessanta ragazzi: attori, filmmakers, registi o
             semplici appassionati
           </p>

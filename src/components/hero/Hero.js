d21da1e31a9ce26a9c2c8db89853088f5a94f2f7
SRFeesha <SRFeesha@gmail.com>
2020-09-02 19:08:06 +0200

    retrying

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import style from "./Hero.module.css";
 import Spacer from "../Spacer";
 import { gsap } from "gsap"
-// import Splitting from "splitting";
+import Splitting from "splitting";
 // import 'splitting/dist/splitting.css'
 // import 'splitting/dist/splitting-cells.css'
 
@@ -16,22 +16,17 @@ const Hero = () => {
 
   useEffect(() => {
 
-    if (titleRef) {
       // if (typeof window !== "undefined") {
 
-      // let results = Splitting({target: titleRef, by: 'lines',})
-      
-      // results.forEach((splitResult) => {
-      //   const wrappedLines = splitResult.lines.map((wordsArr) => `
-      //   <div class="mask-line">
-      //     ${wordsArr.map((word) => `${word.outerHTML}<span class="whitespace"> 
-      //    </span>`).join('')}
-      //   </div>`).join('');
-      //   splitResult.el.innerHTML = wrappedLines;
-      // });
-
-
-    }
+      let results = Splitting({target: titleRef, by: 'lines',})
+      results.forEach((splitResult) => {
+        const wrappedLines = splitResult.lines.map((wordsArr) => `
+        <div class="mask-line">
+          ${wordsArr.map((word) => `${word.outerHTML}<span class="whitespace"> 
+         </span>`).join('')}
+        </div>`).join('');
+        splitResult.el.innerHTML = wrappedLines;
+      });
   }, []);
 
   useEffect(() => {
@@ -47,10 +42,10 @@ const Hero = () => {
     tl.from(imgContainerRef, 1.2, { y: 1280, ease: "ease-out" })
       .from(hero, 2, { scale: 1.6, ease: "ease-out" }, .2)
       .to(titleRef, 0, { autoAlpha: 1}, 1)
-      .staggerFrom(lineRefs, .4, { y: 100, ease: 'ease-out', autoAlpha: 0 }, .05, 1)
-      .fromTo(subtitleRef, .3, {yPercent: 50, ease: 'ease-out', autoAlpha: 0}, {yPercent: 0, autoAlpha: .5}, 1.5)
+      .staggerFrom(lineRefs, .4, { y: 100, ease: 'ease-out', autoAlpha: 0 }, .1, 1)
+      .fromTo(subtitleRef, .3, {yPercent: 50, ease: 'ease-out', autoAlpha: 0}, {yPercent: 0, autoAlpha: .5}, 2.1)
 
-    // tl.to(titleRef,0,{autoAlpha:1})
+    // tl.tqo(titleRef,0,{autoAlpha:1})
     // tl.staggerFrom(lineRefs, 0.4, { yPercent: 100, ease: "ease-out", autoAlpha: 0 }, 0.05);
   }, [tl]);
 

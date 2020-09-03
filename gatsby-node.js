21d4bcfd9158755f24d5f73572e085fc2b954ef1
SRFeesha <SRFeesha@gmail.com>
2020-09-03 17:41:42 +0200

    changed from 'path' to 'resolve'

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,7 @@ exports.createPages = ({ actions, graphql }) => {
       createPage({
         path: edge.node.fields.slug,
         tags: edge.node.frontmatter.tags,
-        component: path.resolve(
+        component: require.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
         // additional data can be passed via context
@@ -64,7 +64,7 @@ exports.createPages = ({ actions, graphql }) => {
 
       createPage({
         path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
+        component: require.resolve(`src/templates/tags.js`),
         context: {
           tag,
         },

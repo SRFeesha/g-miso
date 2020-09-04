4ea96a5eb47e33761123c5338208e12a8bbaf05a
SRFeesha <SRFeesha@gmail.com>
2020-09-04 10:59:42 +0200

    added inline-delay

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-iubenda-cookie-footer',
       options: {
-        iubendaOptions: {"lang":"it","siteId":1999025,"cookiePolicyId":52804844, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"acceptButtonColor":"#0073CE","acceptButtonCaptionColor":"white","customizeButtonColor":"#212121","customizeButtonCaptionColor":"white","rejectButtonColor":"#0073CE","rejectButtonCaptionColor":"white","closeButtonDisplay":false,"position":"float-top-left","textColor":"white","backgroundColor":"rgba(173.14, 172.06, 172.06, 0.34)" } },
+        iubendaOptions: {"lang":"it","siteId":1999025,"cookiePolicyId":52804844, "cookiePolicyInOtherWindow":true,"inlineDelay":3500, "banner":{ "acceptButtonDisplay":true,"customizeButtonDisplay":true,"acceptButtonColor":"#0073CE","acceptButtonCaptionColor":"white","customizeButtonColor":"#212121","customizeButtonCaptionColor":"white","rejectButtonColor":"#0073CE","rejectButtonCaptionColor":"white","closeButtonDisplay":false,"position":"float-top-left","textColor":"white","backgroundColor":"rgba(173.14, 172.06, 172.06, 0.34)" } },
       }
     },
     {

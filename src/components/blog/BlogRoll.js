4f0ba9579bd49678fcc6e8f1031e4a78a54a2ec8
Beniamino Marini <beniaminomarini@Beniaminos-MBP.lan>
2020-10-24 19:35:27 +0200

    Removed old CMS

--- src/components/blog/BlogRoll.js
+++ src/components/blog/BlogRoll.js
@@ -1,82 +1,63 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import style from './blog.module.css'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { Link, graphql, StaticQuery } from 'gatsby'
+// import style from './blog.module.css'
 
-class BlogRoll extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+// const BlogRoll = ({ data }) => {
+//   // const { data } = this.props
+//   const { edges: posts } = data.allContentfulBlogPost
+//   console.log(data);
 
-    return (
-      <div id="blog-post-list">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <Link className={style.link} to={post.fields.slug}>
-              <article className={style.blogPost}>
-                <h3 className={style.title}>{post.frontmatter.title}</h3>
-                <p className={style.subtitle}>  
-                  {post.frontmatter.date}
-                  <span className="transparent">
-                    <span className={style.space}> /</span>
-                  </span>
-                  {/* <span className="transparent">Autore: </span> */}
-                  {/* {post.frontmatter.author}Ezio */}
-                  <span className="transparent">
-                    {/* <span className={style.space}>/</span>  */}
-                    Tempo di lettura:{' '}
-                  </span>
-                  {post.timeToRead} min{' '}
-                </p>
-                <p class={style.excerpt}>{post.excerpt}</p>
-              </article>
-            </Link>
-          ))}
-      </div>
-    )
-  }
-}
+//   return (
+//     <div id="blog-post-list">
+//       {posts &&
+//         posts.map(({ node: post }) => (
+//           <Link key={post.id} className={style.link} to={post.slug}>
+//             {console.log(post.id)}
+//             <article className={style.blogPost}>
+//               <h3 className={style.title}>{post.title}</h3>
+//               <p className={style.subtitle}>  
+//                   {post.date}
+//                   <span className="transparent">
+//                     <span className={style.space}> /</span>
+//                   </span>
+//                   <span className="transparent">Autore: </span>
+//                   {post.frontmatter.author}Ezio
+//                   <span className="transparent">
+//                     <span className={style.space}>/</span> 
+//                     Tempo di lettura:{' '}
+//                   </span>
+//                   {post.timeToRead} min{' '}
+//                 </p>
+//               <p className={style.excerpt}>{post.excerpt}</p>
+//             </article>
+//           </Link>
+//         ))}
+//     </div>
+//   )
+// }
 
-BlogRoll.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+// export default BlogRoll;
 
-export default () => (
-  <StaticQuery
-    query={graphql`
-      query BlogRollQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: {
-            frontmatter: {
-              templateKey: { eq: "blog-post" }
-              featuredpost: { ne: true }
-            }
-          }
-        ) {
-          edges {
-            node {
-              excerpt(pruneLength: 190)
-              id
-              timeToRead
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                templateKey
-                date(formatString: "D MMM YYYY", locale: "IT")
-                featuredpost
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data, count) => <BlogRoll data={data} count={count} />}
-  />
-)
+// export const pageQuery = graphql`
+//   {
+//     allContentfulBlogPost {
+//       edges {
+//         node {
+//           id
+//           slug
+//           title
+//           tags
+//           publishDate(locale: "IT")
+//           body {
+//             body
+//           }
+//           author {
+//             id
+//             name
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
--- src/components/blog/FeaturedPost.js
+++ src/components/blog/FeaturedPost.js
@@ -1,88 +1,88 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import style from './blog.module.css'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { Link, graphql, StaticQuery } from 'gatsby'
+// import style from './blog.module.css'
 
-class FeaturedPost extends React.Component {
-  render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
+// class FeaturedPost extends React.Component {
+//   render() {
+//     const { data } = this.props
+//     const { edges: posts } = data.allMarkdownRemark
 
-    return (
-      <div id="featured-posts">
-        {posts &&
-          posts.map(({ node: post }) => (
-            <Link className={style.link} to={post.fields.slug} key={post.id}>
-              <article className={style.featuredPost}>
-                <p className={style.data}>
-                  <span className={style.day}>
-                    {post.frontmatter.giorno} <br />
-                  </span>
-                  {post.frontmatter.meseanno}
-                </p>
-                <div className="content">
-                  <h2>{post.frontmatter.title}</h2>
-                  <p className={style.subtitle}>
-                    <span className="autore">
-                      <span className="transparent">Autore: </span>
-                      {/* {post.frontmatter.author} */}Ezio
-                    </span>
-                    <span className="transparent">
-                      {/* <span className={style.space}>/</span>  */}
-                      Tempo di lettura:{' '}{post.timeToRead} min
-                    </span>
-                  </p>
-                  <p>{post.excerpt}</p>
-                </div>
-              </article>
-            </Link>
-          ))}
-      </div>
-    )
-  }
-}
+//     return (
+//       <div id="featured-posts">
+//         {posts &&
+//           posts.map(({ node: post }) => (
+//             <Link className={style.link} to={post.fields.slug} key={post.id}>
+//               <article className={style.featuredPost}>
+//                 <p className={style.data}>
+//                   <span className={style.day}>
+//                     {post.frontmatter.giorno} <br />
+//                   </span>
+//                   {post.frontmatter.meseanno}
+//                 </p>
+//                 <div className="content">
+//                   <h2>{post.frontmatter.title}</h2>
+//                   <p className={style.subtitle}>
+//                     <span className="autore">
+//                       <span className="transparent">Autore: </span>
+//                       {/* {post.frontmatter.author} */}Ezio
+//                     </span>
+//                     <span className="transparent">
+//                       {/* <span className={style.space}>/</span>  */}
+//                       Tempo di lettura:{' '}{post.timeToRead} min
+//                     </span>
+//                   </p>
+//                   <p>{post.excerpt}</p>
+//                 </div>
+//               </article>
+//             </Link>
+//           ))}
+//       </div>
+//     )
+//   }
+// }
 
-FeaturedPost.propTypes = {
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      edges: PropTypes.array,
-    }),
-  }),
-}
+// FeaturedPost.propTypes = {
+//   data: PropTypes.shape({
+//     allMarkdownRemark: PropTypes.shape({
+//       edges: PropTypes.array,
+//     }),
+//   }),
+// }
 
-export default () => (
-  <StaticQuery
-    query={graphql`
-      query FeaturedPostQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          filter: {
-            frontmatter: {
-              featuredpost: { eq: true }
-              templateKey: { eq: "blog-post" }
-            }
-          }
-        ) {
-          edges {
-            node {
-              excerpt(pruneLength: 190)
-              id
-              timeToRead
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-                templateKey
-                meseanno: date(formatString: "MMM YYYY", locale: "IT")
-                giorno: date(formatString: "DD", locale: "IT")
-                featuredpost
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={(data, count) => <FeaturedPost data={data} count={count} />}
-  />
-)
+// export default () => (
+//   <StaticQuery
+//     query={graphql`
+//       query FeaturedPostQuery {
+//         allMarkdownRemark(
+//           sort: { order: DESC, fields: [frontmatter___date] }
+//           filter: {
+//             frontmatter: {
+//               featuredpost: { eq: true }
+//               templateKey: { eq: "blog-post" }
+//             }
+//           }
+//         ) {
+//           edges {
+//             node {
+//               excerpt(pruneLength: 190)
+//               id
+//               timeToRead
+//               fields {
+//                 slug
+//               }
+//               frontmatter {
+//                 title
+//                 templateKey
+//                 meseanno: date(formatString: "MMM YYYY", locale: "IT")
+//                 giorno: date(formatString: "DD", locale: "IT")
+//                 featuredpost
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `}
+//     render={(data, count) => <FeaturedPost data={data} count={count} />}
+//   />
+// )
